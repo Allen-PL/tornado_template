73c6806f9cc7d8b18948c6466ae5dc74f3c852fd
Lin Peng <pl@yunpankeji.com>
2021-10-18 18:31:58 +0800

    init

--- src/handlers/http_handler/main_handler.py
+++ src/handlers/http_handler/main_handler.py
@@ -4,21 +4,24 @@
 # @Author: pl
 # @Time: 2021/10/15 9:20
 # from utils.authenticated import login_authentication
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from handlers.base_handler import BaseHandler
 from common.exceptions import ApiException
+from models.base import get_db_session, User
 from utils.web_log import get_logger
 
 
 class MainHandler(BaseHandler):
 
     # @login_authentication
-    def get(self):
-        a = 1
-        a[0]
-        get_logger().exception("123456789123456798")
-        info = {'name': 'mast be a int'}
-        return self.response_data(info)
+    async def get(self, db_session: AsyncSession = get_db_session):
+        async with db_session.begin():
+            results = await get_db_session().execute(select(User).limit(10))
+        data = results.scalars().all()
+        print(data)
+        return self.response_data(data)
 
     def post(self):
         print(self.data.get('name'))
@@ -26,3 +29,4 @@ class MainHandler(BaseHandler):
 
     def put(self):
         return self.response_data('ok')
+
